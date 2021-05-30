a40e33caf8e13fe8ca07b3cf25eea2c05f2403b3
Oscar De Leon <owdelc@gmail.com>
2021-05-30 12:53:10 -0600

    configuracion lista

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render(){
+        return(
+            <div className='Hola'>
+                Hola Mundo!
+            </div>
+        );
+    }
+}
+
+export default App;
